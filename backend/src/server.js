3a932d2345ef1db6bfee10904e5bc50b5533f1b8
24-arushi-saxena <arushisaxena544@gmail.com>
2026-02-24 03:05:39 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 
 import notesRoutes from './routes/notesRoutes.js';
@@ -13,12 +14,18 @@ dotenv.config();
 
 const PORT= process.env.PORT || 3000;
 const app= express();
+const __dirname=path.resolve();
 
-app.use(
+
+//middleware
+if(process.env.NODE_ENV !== "production" ){
+    app.use(
   cors({
     origin: 'http://localhost:5173',
-  }));
-//middleware
+  })
+);
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -26,6 +33,16 @@ app.use(rateLimiter);
 
 app.use("/api/notes",notesRoutes);
 
+
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // catch-all route for SPA – use a regex to avoid path-to-regexp parsing errors
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 const startServer = async () => {
